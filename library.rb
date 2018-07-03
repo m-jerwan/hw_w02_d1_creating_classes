9855d7c49e602c1bf51f9c07184bb8c7a7ea45e3
Marcin Jerwan <m.jerwan@gmail.com>
2018-07-03 20:34:32 +0100

    Part A, B and extension DONE

--- library.rb
+++ library.rb
@@ -1,5 +1,56 @@
 
 class Library
 
+attr_reader :books
+
+def initialize(arg_1)
+  @books  =  arg_1
+end
+
+#----------------------------------------------
+
+def get_book_info_by_title(title)
+  for book in books
+    if book[:title] == title
+       return book
+    end
+  end
+end
+
+def get_book_rental_details_by_title(title)
+  for book in books
+    if book[:title] == title
+       return book[:rental_details]
+    end
+  end
+end
+
+
+def add_book_to_library(title)
+  @books << {
+    title: "#{title}",
+    rental_details: {
+      student_name: "",
+      due_date: ""
+    }}
+
+end
+
+def change_rental_details(title, student_name, due_date)
+  for book in books
+    if book[:title] == title
+       book[:rental_details][:student_name] = student_name
+       book[:rental_details][:due_date] = due_date
+    end
+  end
+
+end
+
+
+
+
+
+
+
 
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -16,15 +16,66 @@ class TestLibrary < MiniTest::Test
         rental_details: {
           student_name: "Jonathan",
           due_date: "01/01/16"
-        }},
-      {
-        title: "",
-        rental_details: {
-          student_name: "",
-          due_date: ""
         }}
     ]
 
     @library = Library.new(@books)
 
   end
+
+  def test_get_books
+    assert_equal(@books, @library.books)
+  end
+
+  def test_get_book_info_by_title
+    expected = {
+      title: "Necronomicon",
+      rental_details: {
+        student_name: "Jonathan",
+        due_date: "01/01/16"
+      }}
+    actual = @library.get_book_info_by_title("Necronomicon")
+    assert_equal(expected, actual)
+  end
+
+
+  def test_get_book_rental_details_by_title
+    expected = {
+      student_name: "Angelina",
+      due_date: "11/04/16"
+    }
+    actual = @library.get_book_rental_details_by_title("Lord of the Flies")
+    assert_equal(expected, actual)
+  end
+
+  def test_add_book_to_library
+    @library.add_book_to_library("Pan Tadeusz")
+    expected = {
+        title: "Pan Tadeusz",
+        rental_details: {
+          student_name: "",
+          due_date: ""
+        }}
+    actual = @library.get_book_info_by_title("Pan Tadeusz")
+    assert_equal(expected, actual)
+  end
+
+
+  def test_change_rental_details
+    @library.change_rental_details("Necronomicon", "Alan Poe", "11/11/18")
+    expected = {
+      title: "Necronomicon",
+      rental_details: {
+        student_name: "Alan Poe",
+        due_date: "11/11/18"
+      }}
+    actual = @library.get_book_info_by_title("Necronomicon")
+    assert_equal(expected, actual)
+  end
+
+
+
+
+
+
+end
